ce164aa8971fed322830dd1e7259d97cf061ba06
adidonner <126164933+adidonner@users.noreply.github.com>
2025-06-03 17:38:48 +0300

    Add files via upload

--- coupons3/app/page.tsx
+++ coupons3/app/page.tsx
@@ -2,7 +2,8 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
-import CashBackLogo from '@/app/ui/cash-back-logo';
+import CashBackLogo from '@/app/ui/cash-back-logo'
+
 
 export default function Page() {
   return (
